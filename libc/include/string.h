e4179c3f0b7ef83d584df6ff283d4455f6af812e
Arjun Korrakuti <110309029+arjunkorrakuti@users.noreply.github.com>
2026-05-19 10:56:00 -0400

    Support overlapping memory moves with directional copying

--- libc/include/string.h
+++ libc/include/string.h
@@ -10,6 +10,8 @@ void* memcpy(void* destination, const void* source, size_t size);
 
 void* memset(void* destination, int value, size_t size);
 
+void* memmove(void* destination, const void* source, size_t size);
+
 #ifdef __cplusplus
 }
 #endif
--- libc/string.cpp
+++ libc/string.cpp
@@ -17,3 +17,16 @@ extern "C" void* memset(void* destination, int value, size_t size) {
   }
   return destination;
 }
+
+extern "C" void* memmove(void* destination, const void* source, size_t size) {
+  auto* output = static_cast<unsigned char*>(destination);
+  const auto* input = static_cast<const unsigned char*>(source);
+  if (reinterpret_cast<uintptr_t>(output) < reinterpret_cast<uintptr_t>(input)) {
+    return memcpy(destination, source, size);
+  }
+  while (size != 0) {
+    size--;
+    output[size] = input[size];
+  }
+  return destination;
+}
