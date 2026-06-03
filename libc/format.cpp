69aa50a5a43df9a4c0cf05ceb8de05b4988ec741
Arjun Korrakuti <110309029+arjunkorrakuti@users.noreply.github.com>
2026-06-02 21:52:00 -0400

    Convert unsigned integers to decimal text without allocation

--- libc/format.cpp
+++ libc/format.cpp
@@ -25,6 +25,18 @@ size_t format::write(Writer writer, void* context, const char* pattern,
         emit(*string++);
       }
       pattern++;
+    } else if (*pattern == 'u') {
+      unsigned int value = va_arg(arguments, unsigned int);
+      char buffer[10];
+      size_t size = 0;
+      do {
+        buffer[size++] = static_cast<char>('0' + value % 10);
+        value /= 10;
+      } while (value != 0);
+      while (size != 0) {
+        emit(buffer[--size]);
+      }
+      pattern++;
     } else if (*pattern == 'c') {
       emit(static_cast<char>(va_arg(arguments, int)));
       pattern++;
